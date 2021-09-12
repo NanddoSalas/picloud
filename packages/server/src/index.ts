877a78610e4f15f0b5a5cc84184d74942b1fa669
Fernando Salas <nanddosalas@gmail.com>
2021-09-12 11:47:16 -0600

    init connectino, app, server and httpServe

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,1 +1,45 @@
-console.log('Hello world!');
+import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
+import { ApolloServer } from 'apollo-server-express';
+import dotenv from 'dotenv';
+import express from 'express';
+import { createServer } from 'http';
+import path from 'path';
+import 'reflect-metadata';
+import { buildSchema } from 'type-graphql';
+import { createConnection, getConnectionOptions } from 'typeorm';
+
+dotenv.config();
+
+const main = async () => {
+  const connectionOptions = await getConnectionOptions();
+
+  Object.assign(connectionOptions, {
+    sincronize: true,
+    entities: [path.join(__dirname, '/entity/**/*.{ts,js}')],
+  });
+
+  await createConnection();
+
+  const app = express();
+
+  const httpServer = createServer(app);
+
+  const schema = await buildSchema({
+    resolvers: [path.join(__dirname, '/modules/**/*.resolver.{ts,js}')],
+  });
+
+  const server = new ApolloServer({
+    schema,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
+
+  await server.start();
+
+  server.applyMiddleware({ app });
+
+  httpServer.listen(3000, () => {
+    console.log('Listening at http://localhost:3000/graphql');
+  });
+};
+
+main().catch((err) => console.error(err));
