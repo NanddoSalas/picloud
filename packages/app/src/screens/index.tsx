76606e91f9f0b0e78ec566256c804cba0131799d
Fernando Salas <nanddosalas@gmail.com>
2021-09-21 16:48:40 -0600

    add screens index

---
+++ packages/app/src/screens/index.tsx
@@ -0,0 +1,8 @@
+import Account from './Account';
+import Folder from './Folder';
+import Gallery from './Gallery';
+import Login from './Login';
+import Photos from './Photos';
+import Signup from './Signup';
+
+export { Login, Signup, Account, Photos, Gallery, Folder };
