a766beb7b0394cc5f5e7d630cb339b4286e0d3e0
Fernando Salas <nanddosalas@gmail.com>
2021-09-28 00:23:07 -0600

    update types
     * remove deprecated types
     * update StackParams type

--- packages/app/src/types.ts
+++ packages/app/src/types.ts
@@ -1,16 +1,11 @@
-export type Photo = {
-  id: number;
-  createdAt: number;
-  uri: string;
-};
-
 export type StackParams = {
   Login: undefined;
   Signup: undefined;
   Main: undefined;
   Account: undefined;
-  Folder: {
-    name: string;
+  Album: {
+    albumId: string;
+    albumName: string;
   };
 };
 
