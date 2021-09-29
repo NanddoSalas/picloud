5c835cbebf7b0f3356c5977cb470f5ea22ed661c
Fernando Salas <nanddosalas@gmail.com>
2021-09-28 19:58:46 -0600

    update App component

--- packages/app/src/index.tsx
+++ packages/app/src/index.tsx
@@ -1,22 +1,12 @@
 import { ApolloProvider } from '@apollo/client';
 import React from 'react';
 import client from './client';
-import GalleryContext from './context/GalleryContext';
-import useGallery from './hooks/useGallery';
 import Navigation from './Navigation';
 
-const App = () => {
-  const { folders, getBackground, getPhotos, reload } = useGallery();
-
-  return (
-    <ApolloProvider client={client}>
-      <GalleryContext.Provider
-        value={{ folders, getBackground, getPhotos, reload }}
-      >
-        <Navigation />
-      </GalleryContext.Provider>
-    </ApolloProvider>
-  );
-};
+const App = () => (
+  <ApolloProvider client={client}>
+    <Navigation />
+  </ApolloProvider>
+);
 
 export default App;
