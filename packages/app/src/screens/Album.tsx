d22b5bfffaefd28c8318148307a8f3e5cc23fb49
Fernando Salas <nanddosalas@gmail.com>
2021-10-11 07:49:56 -0600

    update Album screen

--- packages/app/src/screens/Album.tsx
+++ packages/app/src/screens/Album.tsx
@@ -4,12 +4,12 @@ import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import * as Haptics from 'expo-haptics';
 import * as MediaLibrary from 'expo-media-library';
 import { Box } from 'native-base';
-import React, { useContext, useEffect, useLayoutEffect, useState } from 'react';
-import { FlatList, useWindowDimensions } from 'react-native';
+import React, { useContext, useLayoutEffect } from 'react';
 import ImageView from 'react-native-image-viewing';
-import Asset from '../components/Asset';
+import AssetsList from '../components/AssetsList';
 import SelectionHeader from '../components/SelectionHeader';
 import BackupContext from '../context/BackupContext';
+import useAlbum from '../hooks/useAlbum';
 import useImageView from '../hooks/useImageView';
 import useSelection from '../hooks/useSelection';
 import { StackParams } from '../types';
@@ -20,8 +20,7 @@ const Album: React.FC<NativeStackScreenProps<StackParams, 'Album'>> = ({
 }) => {
   const { colors } = useTheme();
   const { albumId, albumName } = route.params;
-  const [assets, setAssets] = useState<MediaLibrary.Asset[]>([]);
-  const { width } = useWindowDimensions();
+  const { assets, loadNextPage } = useAlbum(albumId);
   const { backUpAssets } = useContext(BackupContext);
   const {
     isSelectionEnabled,
@@ -80,75 +79,16 @@ const Album: React.FC<NativeStackScreenProps<StackParams, 'Album'>> = ({
     });
   }, [isSelectionEnabled, selectedItems]);
 
-  useEffect(() => {
-    const getAssets = async (cursor?: string) => {
-      const pageInfo = await MediaLibrary.getAssetsAsync({
-        album: albumId,
-        mediaType: 'photo',
-        after: cursor,
-      });
-
-      setAssets((current) => [...current, ...pageInfo.assets]);
-
-      if (pageInfo.hasNextPage) getAssets(pageInfo.endCursor);
-    };
-
-    getAssets();
-
-    const listener = MediaLibrary.addListener((event) => {
-      if (event.hasIncrementalChanges) {
-        const { deletedAssets, insertedAssets, updatedAssets } = event;
-
-        if (insertedAssets) {
-          setAssets((current) => [...current, ...insertedAssets]);
-        } else if (deletedAssets) {
-          setAssets((current) => {
-            const deletedAssetsId = deletedAssets.map(({ id }) => id);
-
-            const newState = current.filter(
-              ({ id }) => !deletedAssetsId.includes(id),
-            );
-
-            return newState;
-          });
-        } else if (updatedAssets) {
-          setAssets((current) => {
-            const updatedAssetsId = updatedAssets.map(({ id }) => id);
-
-            return current.map((asset) => {
-              const index = updatedAssetsId.indexOf(asset.id);
-
-              if (index) return updatedAssets[index];
-
-              return asset;
-            });
-          });
-        }
-      } else {
-        setAssets([]);
-        getAssets();
-      }
-    });
-
-    return listener.remove;
-  }, []);
-
-  const handlePress = (id: string, index: number) => {
+  const handleAssetPress = (id: string, index: number) => {
     if (isSelectionEnabled) handleSelection(id);
     else showImageView(index);
   };
 
-  const handleLongPress = (id: string) => {
+  const handleAssetLongPress = (id: string) => {
+    Haptics.selectionAsync();
     if (!isSelectionEnabled) enableSelection(id);
     else handleSelection(id);
-    Haptics.selectionAsync();
   };
-
-  const minAssetWidth = 100;
-
-  const assetsPerRow = Math.trunc(width / minAssetWidth);
-  const assetsWidth = width - (assetsPerRow - 1);
-  const assetWidth = assetsWidth / assetsPerRow;
 
   return (
     <Box>
@@ -158,32 +98,12 @@ const Album: React.FC<NativeStackScreenProps<StackParams, 'Album'>> = ({
         imageIndex={imageIndex}
         onRequestClose={hiddeImageView}
       />
-      <FlatList
-        data={assets}
-        renderItem={({ item, index }) => {
-          const isSelected = selectedItems.includes(item.id);
-
-          return (
-            <Asset
-              id={item.id}
-              uri={item.uri}
-              index={index}
-              assetWidth={assetWidth}
-              isSelected={isSelected}
-              isSelectionEnabled={isSelectionEnabled}
-              handlePress={handlePress}
-              handleLongPress={handleLongPress}
-            />
-          );
-        }}
-        keyExtractor={(item) => item.id}
-        initialNumToRender={20}
-        numColumns={assetsPerRow}
-        getItemLayout={(data, index) => ({
-          length: assetWidth,
-          offset: assetWidth * index,
-          index,
-        })}
+      <AssetsList
+        assets={assets}
+        selectedAssetsId={selectedItems}
+        onAssetPress={handleAssetPress}
+        onLongAssetPress={handleAssetLongPress}
+        onEndReached={() => loadNextPage()}
       />
     </Box>
   );
